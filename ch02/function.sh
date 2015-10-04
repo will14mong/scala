3bf9bef90dc2d7166ef176ba952593bf178d12bc
William.Ong <will14m.ong@gmail.com>
2015-10-04 19:33:43 +0800

    added ch02 code

---
+++ ch02/function.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+exec scala "$0" "$@"
+!#
+
+// function of type (x: Int, y: Int)Int
+def max(x: Int, y: Int): Int = {
+  if (x > y) x
+  else y
+}
+
+// or we could define it shorter
+def max2(x: Int, y: Int) = if (x>y) x else y
+
+// we have to put parentheses for println
+println(max(10,12))
+println(max2(12,35))
+
+// function without parameter
+def greet() = println("Hello, World from greet")
+def greet2 = println("Hello, World from greet2")
+greet
+greet2
+
+// printing argument, note the use of parentheses (0) instead of sq bracket [0]
+println("Hello, " + args(0) + "!")
+
+// printing list of arguments 
+// args.foreach((arg: String) => println(arg))
+// args.foreach(arg => println(arg))
+// args.foreach(println _)
+println("Printing using foreach")
+args.foreach(println)
+
+println("Printing using for")
+for (arg <- args) 
+  println(arg)
