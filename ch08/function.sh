c591864768273523cfdc933ac043dad66ec2fc8c
William.Ong <will14m.ong@gmail.com>
2015-10-05 23:19:33 +0800

    update content

--- ch08/function.sh
+++ ch08/function.sh
@@ -37,3 +37,28 @@ println(a(1,2,3))
 println(b(4))
 
 // closures
+def makeIncreaser(more: Int) = (x: Int) => x + more
+val inc1 = makeIncreaser(1)
+val inc9999 = makeIncreaser(9999)
+println(inc1(10))
+println(inc9999(10))
+
+// repeated parameters
+def echo(args: String*) =
+  for (arg <- args) println(arg)
+echo("hello","world")
+
+val arr = Array("What's","up","doc?")
+// tells the compiler to pass each element of arr as its own argument to echo, rather than all of it as a single arg.
+echo(arr: _*)
+
+// named arguments
+def speed(distance: Float, time: Float): Float = distance / time
+// we can switch order with named arg
+speed(time = 10, distance = 100)
+
+// default parameter value
+def printTime(out: java.io.PrintStream = Console.out, divisor: Int = 1) = 
+  out.println("time = " + System.currentTimeMillis()/divisor)
+printTime(out = Console.err)
+printTime(divisor = 1000)
