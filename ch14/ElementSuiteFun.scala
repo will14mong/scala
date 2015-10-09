a15a72975c6e0c1c69d2263e471e1078deddb947
will14mong <will14m.ong@gmail.com>
2015-10-09 17:26:57 +0800

    Update ElementSuiteFun.scala

--- ch14/ElementSuiteFun.scala
+++ ch14/ElementSuiteFun.scala
@@ -28,3 +28,27 @@ Suites: completed 1, aborted 0
 Tests: succeeded 2, failed 0, canceled 0, ignored 0, pending 0
 All tests passed.
 */
+
+/*
+OTHER WAYS
+
+If there's error in the above code, we wouldn't know what's the value, there's another way
+
+assert(ele.width === 2)
+with triple =, when it's failing we would see a message such as "3 did not equal 2" in the failure report
+
+expect(2) {
+  ele.width
+}
+with this expression we indicate that we expect the code between the curly braces to result in 2. so we'll see msg like
+  "Expected 2, but got 3"
+  
+intercept[IllegalArgumentException] {
+  elem('x', -2, 3)
+}
+if we want to check that a method throws an expected exception, we can use the above.
+We'll get something like
+Expected IllegalArgumentException to be thrown,
+   but NegativeArraySizeException was thrown.
+   
+*/
