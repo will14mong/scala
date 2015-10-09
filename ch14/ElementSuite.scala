f6f2b06d1999991831af873f2e110b93aab7cb9a
will14mong <will14m.ong@gmail.com>
2015-10-09 15:12:47 +0800

    Create ElementSuite.scala

---
+++ ch14/ElementSuite.scala
@@ -0,0 +1,17 @@
+import org.scalatest.Suite
+import Element.elem
+
+class ElementSuite extends Suite {
+  def testUniformElement() {
+    val ele = elem('x', 2, 3)
+    assert(ele.width == 2)
+  }
+}
+
+/*
+Compilations step:
+scalac -cp scalatest_2.11-2.2.4.jar Element.scala ElementSuite.scala
+
+Running step:
+scala -cp scalatest_2.11-2.2.4.jar org.scalatest.run ElementSuite
+*/
