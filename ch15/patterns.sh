e458123e20dc13fef18f7d6de18226b30abbeac6
will14mong <will14m.ong@gmail.com>
2015-10-13 10:39:41 +0800

    Update patterns.sh

--- ch15/patterns.sh
+++ ch15/patterns.sh
@@ -50,6 +50,6 @@ def simplifyAll(expr: Expr): Expr = expr match {
   case BinOp("+", e, Number(0)) => simplifyAll(e)       // '0' is a neutral element for '+'
   case BinOp("*", e, Number(1)) => simplifyAll(e)       // '1' is a neutral element for '*'
   case UnOp(op, e) => UnOp(op, simplifyAll(e))
-  case BinOp(op, l, r) => BinOp(op, l, r) => BinOp(op, simplifyAll(l), simplifyAll(r))
+  case BinOp(op, l, r) => BinOp(op, simplifyAll(l), simplifyAll(r))
   case _ => expr
 }
